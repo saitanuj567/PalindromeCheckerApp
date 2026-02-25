341be4ae8b82b694cb9e485f02fafa636da26bb2
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 10:01:58 +0530

    UC1: Add application entry and welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,20 @@
-public class PalindromeCheckerApp {
-}
+public class gir {
+
+    /**
+     * MAIN CLASS - UseCase1PalindromeCheckerApp
+     *
+     * Use Case 1: Application Entry & Welcome Message
+     */
+
+
+        /**
+         * Application entry point.
+         */
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to the Palindrome Checker Management System");
+            System.out.println("Version : 1.0");
+            System.out.println("System initialized successfully.");
+        }
+    }
+
