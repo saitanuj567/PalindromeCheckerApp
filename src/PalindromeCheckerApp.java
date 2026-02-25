c6577894dae6025a72612b1a8eeb44d5e538eccd
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 11:07:31 +0530

    UC8: Recursive palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,35 @@
+/**
+ * MAIN CLASS - UseCase9PalindromeCheckerApp
+ *
+ * Use Case 9: Recursive Palindrome Checker
+ */
+
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    /**
+     * Recursive palindrome check
+     */
+    private static boolean check(String s, int start, int end) {
+
+        // ⭐ Base condition
+        if (start >= end)
+            return true;
+
+        // ⭐ Mismatch condition
+        if (s.charAt(start) != s.charAt(end))
+            return false;
+
+        // ⭐ Recursive call
+        return check(s, start + 1, end - 1);
+    }
 }
