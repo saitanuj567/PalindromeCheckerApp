f776bbb00905183bf0ba7cfbf4904592be3368a2
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 10:50:16 +0530

    UC7: Deque-based palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,39 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+/**
+ * MAIN CLASS - UseCase7PalindromeCheckerApp
+ *
+ * Use Case 7: Deque-Based Palindrome Check
+ */
+
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // ⭐ Hardcoded input
+        String input = "refer";
+
+        // ⭐ Create deque
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // ⭐ Add characters to deque
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // ⭐ Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // ⭐ Display result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
