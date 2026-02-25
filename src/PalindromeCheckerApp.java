058c227125005458f8fd2fd73ab0dacd910ea106
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 10:45:34 +0530

    UC6: Queue and stack based palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,44 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * MAIN CLASS - UseCase6PalindromeCheckerApp
+ *
+ * Use Case 6: Queue + Stack Based Palindrome Check
+ */
+
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // ⭐ Hardcoded input
+        String input = "civic";
+
+        // ⭐ Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // ⭐ Stack (LIFO)
+        Stack<Character> stack = new Stack<>();
+
+        // ⭐ Insert characters into both
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // ⭐ Compare until queue empty
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // ⭐ Display result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
