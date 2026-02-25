b16424b3ceb76b5eda94e4b1bd6033340860d5d6
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 11:01:03 +0530

    UC8: Recursive palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,38 @@
-public class PalindromeCheckerApp {
+import java.util.LinkedList;
+
+/**
+ * MAIN CLASS - UseCase8PalindromeCheckerApp
+ *
+ * Use Case 8: LinkedList-Based Palindrome Check
+ */
+
+public class PalindromeCheckerApp{
+
+    public static void main(String[] args) {
+
+        // ⭐ Hardcoded input
+        String input = "level";
+
+        // ⭐ Create LinkedList
+        LinkedList<Character> list = new LinkedList<>();
+
+        // ⭐ Add characters to list
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // ⭐ Compare first and last elements
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // ⭐ Display result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
