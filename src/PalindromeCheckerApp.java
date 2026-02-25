cffdfeea8c24283f14462d748a99027d19abf93a
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 10:41:11 +0530

    UC5: Stack-based palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,38 @@
+import java.util.Stack;
+
+/**
+ * MAIN CLASS - UseCase5PalindromeCheckerApp
+ *
+ * Use Case 5: Stack-Based Palindrome Check
+ */
+
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // ⭐ Hardcoded input
+        String input = "noon";
+
+        // ⭐ Create stack
+        Stack<Character> stack = new Stack<>();
+
+        // ⭐ Push characters into stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // ⭐ Compare with popped characters
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // ⭐ Display result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
