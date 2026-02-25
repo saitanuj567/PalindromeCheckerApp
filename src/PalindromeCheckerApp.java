133692968f5044916484098cd51ceecffb63ee67
tarun656 <tharunkumar4976@gmail.com>
2026-02-25 10:23:31 +0530

    UC3: Reverse string based palindrome validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,21 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";   // hardcoded string
+        String reversed = "";
+
+        // reverse string
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        // compare original and reversed
+        boolean isPalindrome = input.equals(reversed);
+
+        // display result
+        System.out.println("Input text: " + input);
+        System.out.println("Reversed text: " + reversed);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
 }
